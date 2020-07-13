9f203073d3d4a9fcd65e71fbd82d435d77a10a4c
lukious <zhsjzhsj@gmail.com>
2020-07-13 19:48:51 +0900

    Update HW#1

---
+++ Homework/Homework#1/main.py
@@ -0,0 +1,26 @@
+####################################################################################
+# File Name :                                                                      #  
+# Date  : 2020/00/00                                                               #  
+# OS : Windows 10                                                                  #  
+# Author :                                                                         #
+# -------------------------------------------------------------------------------  #  
+# requirements : python 3.x                                                        #
+#                                                                                  #
+####################################################################################   
+
+import random                      
+import numpy as np                 
+import pandas as pd                 
+import matplotlib.pyplot as plt    
+import warnings                     
+
+try:
+    from sklearn.cluster import KMeans  # check installation of sklearn
+except:
+    print("Not installed scikit-learn.") 
+    pass
+
+
+if __name__ == '__main__': # Start from main
+    #Implement Load and Plot Code here
+    plt.show() # show plot
